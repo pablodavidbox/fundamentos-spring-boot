bb749e49b0021d71ab93d8879f41b899d7a1d690
Pablo David Schvager <pablodavidseller@hotmail.com>
2022-03-15 17:56:57 -0300

    Add the interface and other class

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,10 +1,17 @@
 package com.fundamentosplatzi.springboot.fundamentos;
 
+import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FundamentosApplication {
+
+	private ComponentDependency componentDependency;
+
+	public FundamentosApplication(ComponentDependency componentDependency){
+		this.componentDependency = componentDependency;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
